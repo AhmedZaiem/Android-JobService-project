b1475647281acd27cfb5ab9fca2d18529d6465a1
CodeBuilder22 <ahmednasrism@gmail.com>
2025-12-05 19:56:51 +0100

    same fix

--- app/src/main/java/com/example/projet/MainActivity.kt
+++ app/src/main/java/com/example/projet/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.projet
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        // Pass the new deep link intent to the NavController
+        findNavController(R.id.nav_host_fragment).handleDeepLink(intent)
     }
 }
